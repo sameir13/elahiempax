6e30324564908645e5c59b324287c1a2cf06f4de
Ghulam Qadir <connecttoqadir@gmail.com>
2023-11-24 21:08:58 +0500

    navbar updated and home page updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,27 +2,19 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-<>
-<div className="navbar">
-    <nav>
-        <div className='logo'>
-<img src="https://www.elahiimpex.com/images/logo-mona.png" alt="Elahi Empex" />
+        <div className="navbar">
+            <nav>
+                <div className='logo'>
+                  <img src="https://www.elahiimpex.com/images/logo-mona.png" alt="Elahi Empex" />
+                </div>
+                    <ul>
+                      <li className="list-item"><a href=''>Home</a></li>
+                      <li className="list-item"><a href=''>About</a></li>
+                      <li className="list-item"><a href=''>Contact</a></li>
+                      <li className="list-item"><a href=''>Products</a></li>
+                    </ul>
+            </nav>
         </div>
-        <div>
-            <ul>
-                <li className="list-item"><a href=''>Home</a></li>
-                <li className="list-item"><a href=''>About</a></li>
-                <li className="list-item"><a href=''>Contact</a></li>
-                <li className="list-item"><a href=''>Products</a></li>
-                <li className="list-item"><a href=''>Home</a></li>
-                <li className="list-item"><a href=''>Home</a></li>
-            </ul>
-        </div>
-    </nav>
-</div>
-
-
-</>
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
 import '@/styles/Navbar.css'
+import '@/styles/Home.css'
 
 
 export default function App({ Component, pageProps }) {
