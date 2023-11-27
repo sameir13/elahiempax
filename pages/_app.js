6dc4d9fd936f7b6dd0727b184fdc78ac0ebe5e17
Ali Shabbbir <alishabbir3151@gmail.com>
2023-11-27 13:13:43 +0500

    Adding about page

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
 import '@/styles/Navbar.css'
+import '@/styles/About.css'
+import '@/styles/Contact.css'
+
 
 
 export default function App({ Component, pageProps }) {
---
+++ pages/about.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const about = () => {
+  return (
+    <>
+      <div className="about">
+        <div className="aboutmain">
+          <div className="aboutimg"></div>
+          <div className="aboutcontent">
+            <h1>About</h1>
+            <p>
+              Founded in 1993, Pem-America, Inc. is now one of America’s fastest
+              growing Home Textile providers. We company began with the
+              production of quilts and has since expanded to include more than
+              50 product lines, distributed through nearly 40 major retailers.
+              As a long-term strategic partner, Pem-America’s innovative product
+              development and creative teams are focused on the creation of
+              impactful private label and branded products, available at key
+              price points. <br /> <br />
+              Pem-America’s corporate office is in the heart of New York City,
+              and operates from multiple distribution centers throughout the
+              United States, thus accommodating business needs of all sizes.
+              Pem-America has exceptional global sourcing and manufacturing
+              capabilities, which allow us to be nimble and respond to ever
+              changing world events, while offering consistent quality and
+              value. Pem-America is continually seeking ways to be more
+              innovative, which includes an increased focus on eco-conscious
+              practices and products.
+            </p>
+            <ul>
+                <li>Rugs</li>
+                <li>Kitchen</li>
+                <li>Cold Weather</li>
+                <li>Utility Bedding</li>
+                <li>Fashion Bedding</li>
+                <li>Bath Accessories</li>
+                <li>Window Treatments</li>
+                <li>Bath & Beach Towels</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default about;
